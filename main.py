a50f15075fb86e52bba6d13b8287a89f181fb9e5
rico ary setiadi <ri.coz.ap87@gmail.com>
2025-08-25 12:47:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from app import app  # noqa: F401
+from app import create_app
+import os
+
+# Gunakan environment variable FLASK_ENV atau default ke 'production'
+environment = os.environ.get("FLASK_ENV", "production")
+
+app = create_app(environment)
